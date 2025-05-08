3da9750e7de5059b4e148421278cdd7a4e534b1f
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-08 15:38:46 -0400

    test

--- ObjOriCarDealership/src/main/java/com/pluralsight/Dealership.java
+++ ObjOriCarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -1,22 +1,17 @@
 package com.pluralsight;
 
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.List;
 
 public class Dealership {
     private String name;
     private String address;
     private String phoneNumber;
 
-    private ArrayList<String> inventory;
-
-    public void GivenCars() {
-        inventory = new ArrayList<>();
-        inventory.add("D & B Used Cars|111 Old Benbrook Rd|817-555-5555");
-        inventory.add("10112|1993|Ford|Explorer|SUV|Red|525123|995.00");
-        inventory.add("37846|2001|Ford|Ranger|truck|Yellow|172544|1995.00");
-        inventory.add("44901|2012|Honda|Civic|SUV|Gray|103221|6995.00");
-        printGivenNames();
-    }
 
     public String getName() {
         return name;
@@ -42,18 +37,5 @@ public class Dealership {
         this.phoneNumber = phoneNumber;
     }
 
-    public ArrayList<String> getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(ArrayList<String> inventory) {
-        this.inventory = inventory;
-    }
-
-    public void printGivenNames() {
-        for (String inventory : inventory) {
-            System.out.println(inventory);
-        }
-    }
 }
 
