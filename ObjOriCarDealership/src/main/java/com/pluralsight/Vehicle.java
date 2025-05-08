0984a78e585b13c54ef7334a2903ebc53d2858a2
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-08 10:34:52 -0400

    created the information for a specific vehicle in vehicle class

--- ObjOriCarDealership/src/main/java/com/pluralsight/Vehicle.java
+++ ObjOriCarDealership/src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
 public class Vehicle {
+    private int vin;
+    private int year;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+
+    public Vehicle(int vin, int year, String model, String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
 }
