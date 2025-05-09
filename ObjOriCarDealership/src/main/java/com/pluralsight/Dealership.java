962f8b2557da0c7040c53413ab33bc0e1eedb3c9
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-09 11:32:43 -0400

    Started adding the start of the array for Dealership.java

--- ObjOriCarDealership/src/main/java/com/pluralsight/Dealership.java
+++ ObjOriCarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -1,35 +1,16 @@
 package com.pluralsight;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Dealership {
     private String name;
     private String address;
     private String phoneNumber;
 
-    public String getName() {
-        return name;
-    }
+    private ArrayList<Vehicle> inventory;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public Dealership(){
+        inventory = new ArrayList<>();
     }
 
 }
