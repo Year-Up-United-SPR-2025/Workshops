8e4f916f9a432f454cb9f2107366fc36471c17e0
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-09 11:50:21 -0400

    added the constructor

--- ObjOriCarDealership/src/main/java/com/pluralsight/Dealership.java
+++ ObjOriCarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -6,12 +6,13 @@ public class Dealership {
     private String name;
     private String address;
     private String phoneNumber;
-
     private ArrayList<Vehicle> inventory;
 
-    public Dealership(){
-        inventory = new ArrayList<>();
+    public Dealership(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.inventory = new ArrayList<>();
     }
-
 }
 
