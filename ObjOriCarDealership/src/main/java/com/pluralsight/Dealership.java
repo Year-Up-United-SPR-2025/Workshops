ea54831744b5bc2efcfbea0ddeea6dad73ca8bad
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-09 12:10:30 -0400

    Added a forgotten string name make into the Vehicle.java class to make the constructor and gets and sets work and added the lists needed for the Dealership.java class

--- ObjOriCarDealership/src/main/java/com/pluralsight/Dealership.java
+++ ObjOriCarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -16,6 +16,9 @@ public class Dealership {
         this.inventory = new ArrayList<>();
     }
 
+    /**
+     * Returns a list of vehicles within the specified price range.
+     */
     public List<Vehicle> getVehiclesByPrice(double min, double max) {
         List<Vehicle> result = new ArrayList<>();
         for (Vehicle v : inventory) {
@@ -25,5 +28,90 @@ public class Dealership {
         }
         return result;
     }
-}
 
+    /**
+     * Returns a list of vehicles that match both the given make and model.
+     */
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle v : inventory) {
+            if (v.getMake().equalsIgnoreCase(make) && v.getModel().equalsIgnoreCase(model)) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns a list of vehicles manufactured within the specified year range.
+     */
+    public List<Vehicle> getVehiclesByYear(int min, int max) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle v : inventory) {
+            if (v.getYear() >= min && v.getYear() <= max) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns a list of vehicles that match the specified color.
+     */
+    public List<Vehicle> getVehiclesByColor(String color) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle v : inventory) {
+            if (v.getColor().equalsIgnoreCase(color)) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns a list of vehicles whose mileage falls within the specified range.
+     */
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle v : inventory) {
+            if (v.getOdometer() >= min && v.getOdometer() <= max) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns a list of vehicles that match the specified type (e.g., sedan, SUV).
+     */
+    public List<Vehicle> getVehiclesByType(String vehicleType) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle v : inventory) {
+            if (v.getVehicleType().equalsIgnoreCase(vehicleType)) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns a list of all vehicles currently in the inventory.
+     */
+    public List<Vehicle> getAllVehicles() {
+        return new ArrayList<>(inventory);
+    }
+
+    /**
+     * Adds a vehicle to the dealership inventory.
+     */
+    public void addVehicle(Vehicle vehicle) {
+        inventory.add(vehicle);
+    }
+
+    /**
+     * Removes a vehicle from the dealership inventory.
+     */
+    public void removeVehicle(Vehicle vehicle) {
+        inventory.remove(vehicle);
+    }
+}
