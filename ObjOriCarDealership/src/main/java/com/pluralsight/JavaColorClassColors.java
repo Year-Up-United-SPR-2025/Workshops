513c13f3b2fca14b706688d507173314ed3e0bb6
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-12 17:00:27 -0400

    added public static final added to colors class to call from later

--- ObjOriCarDealership/src/main/java/com/pluralsight/JavaColorClassColors.java
+++ ObjOriCarDealership/src/main/java/com/pluralsight/JavaColorClassColors.java
@@ -1,56 +1,57 @@
 package com.pluralsight;
+
 import java.awt.Color;
 
 public class JavaColorClassColors {
-    //colors that don't use rgb
-    Color black = Color.BLACK;
-    Color blue = Color.BLUE;
-    Color cyan = Color.CYAN;
-    Color darkGray = Color.DARK_GRAY;
-    Color gray = Color.GRAY;
-    Color green = Color.GREEN;
-    Color lightGray = Color.LIGHT_GRAY;
-    Color magenta = Color.MAGENTA;
-    Color orange = Color.ORANGE;
-    Color pink = Color.PINK;
-    Color red = Color.RED;
-    Color white = Color.WHITE;
-    Color yellow = Color.YELLOW;
+    // colors that don't use rgb
+    public static final Color black = Color.BLACK;
+    public static final Color blue = Color.BLUE;
+    public static final Color cyan = Color.CYAN;
+    public static final Color darkGray = Color.DARK_GRAY;
+    public static final Color gray = Color.GRAY;
+    public static final Color green = Color.GREEN;
+    public static final Color lightGray = Color.LIGHT_GRAY;
+    public static final Color magenta = Color.MAGENTA;
+    public static final Color orange = Color.ORANGE;
+    public static final Color pink = Color.PINK;
+    public static final Color red = Color.RED;
+    public static final Color white = Color.WHITE;
+    public static final Color yellow = Color.YELLOW;
 
-    //colors that use rgb
-    Color skyBlue = new Color(135, 206, 235);
-    Color forestGreen = new Color(34, 139, 34);
-    Color deepPurple = new Color(75, 0, 130);
-    Color gold = new Color(255, 215, 0);
-    Color salmon = new Color(250, 128, 114);
-    Color turquoise = new Color(64, 224, 208);
-    Color midnightBlue = new Color(25, 25, 112);
-    Color tomato = new Color(255, 99, 71);
-    Color violet = new Color(238, 130, 238);
-    Color chocolate = new Color(210, 105, 30);
-    Color lavender = new Color(230, 230, 250);
-    Color beige = new Color(245, 245, 220);
-    Color mintGreen = new Color(152, 255, 152);
-    Color coral = new Color(255, 127, 80);
-    Color teal = new Color(0, 128, 128);
-    Color navy = new Color(0, 0, 128);
-    Color maroon = new Color(128, 0, 0);
-    Color olive = new Color(128, 128, 0);
-    Color lime = new Color(0, 255, 0);
-    Color crimson = new Color(220, 20, 60);
-    Color indigo = new Color(75, 0, 130);
-    Color ivory = new Color(255, 255, 240);
-    Color khaki = new Color(240, 230, 140);
-    Color orchid = new Color(218, 112, 214);
-    Color sienna = new Color(160, 82, 45);
-    Color peru = new Color(205, 133, 63);
-    Color slateGray = new Color(112, 128, 144);
-    Color steelBlue = new Color(70, 130, 180);
-    Color plum = new Color(221, 160, 221);
-    Color wheat = new Color(245, 222, 179);
-    Color skyGray = new Color(192, 192, 192);
-    Color rosyBrown = new Color(188, 143, 143);
-    Color thistle = new Color(216, 191, 216);
-    Color darkOliveGreen = new Color(85, 107, 47);
-    Color lightSalmon = new Color(255, 160, 122);
+    // colors that use rgb
+    public static final Color skyBlue = new Color(135, 206, 235);
+    public static final Color forestGreen = new Color(34, 139, 34);
+    public static final Color deepPurple = new Color(75, 0, 130);
+    public static final Color gold = new Color(255, 215, 0);
+    public static final Color salmon = new Color(250, 128, 114);
+    public static final Color turquoise = new Color(64, 224, 208);
+    public static final Color midnightBlue = new Color(25, 25, 112);
+    public static final Color tomato = new Color(255, 99, 71);
+    public static final Color violet = new Color(238, 130, 238);
+    public static final Color chocolate = new Color(210, 105, 30);
+    public static final Color lavender = new Color(230, 230, 250);
+    public static final Color beige = new Color(245, 245, 220);
+    public static final Color mintGreen = new Color(152, 255, 152);
+    public static final Color coral = new Color(255, 127, 80);
+    public static final Color teal = new Color(0, 128, 128);
+    public static final Color navy = new Color(0, 0, 128);
+    public static final Color maroon = new Color(128, 0, 0);
+    public static final Color olive = new Color(128, 128, 0);
+    public static final Color lime = new Color(0, 255, 0);
+    public static final Color crimson = new Color(220, 20, 60);
+    public static final Color indigo = new Color(75, 0, 130);
+    public static final Color ivory = new Color(255, 255, 240);
+    public static final Color khaki = new Color(240, 230, 140);
+    public static final Color orchid = new Color(218, 112, 214);
+    public static final Color sienna = new Color(160, 82, 45);
+    public static final Color peru = new Color(205, 133, 63);
+    public static final Color slateGray = new Color(112, 128, 144);
+    public static final Color steelBlue = new Color(70, 130, 180);
+    public static final Color plum = new Color(221, 160, 221);
+    public static final Color wheat = new Color(245, 222, 179);
+    public static final Color skyGray = new Color(192, 192, 192);
+    public static final Color rosyBrown = new Color(188, 143, 143);
+    public static final Color thistle = new Color(216, 191, 216);
+    public static final Color darkOliveGreen = new Color(85, 107, 47);
+    public static final Color lightSalmon = new Color(255, 160, 122);
 }
