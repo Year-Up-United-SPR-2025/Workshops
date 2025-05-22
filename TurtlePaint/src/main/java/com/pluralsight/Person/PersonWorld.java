b5950ab23f026e90de37637fc57e82e2ac7543b9
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-22 08:47:21 -0400

    added the turtle project to workshop to work on

---
+++ TurtlePaint/src/main/java/com/pluralsight/Person/PersonWorld.java
@@ -0,0 +1,4 @@
+package com.pluralsight.Person;
+
+public class PersonWorld {
+}
--- TurtlePaint/src/main/java/com/pluralsight/Turtle/World.java
+++ TurtlePaint/src/main/java/com/pluralsight/Turtle/World.java
@@ -1,4 +1,4 @@
-package com.pluralsight;
+package com.pluralsight.Turtle;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
